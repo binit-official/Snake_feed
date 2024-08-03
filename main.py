fe1c77b2f4a65f407b0d58c341b37103937264c8
Binit Kuanar <phantombhaiyaa@gmail.com>
2024-08-03 22:14:30 +0530

    update

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 import pygame
 from pygame import Vector2
 from random import randrange
-
 import config
 
 pygame.init()
 
-# Set up full screen mode
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
-
-# Get the screen size from the display mode
 screen_width, screen_height = pygame.display.get_surface().get_size()
-
-# Update the config variables accordingly
 config.SCREEN_SIZE = screen_width
-config.GRID_CELL_SIZE = screen_width // 80 # Adjust as needed based on the new full-screen size
+config.GRID_CELL_SIZE = screen_width // 80
 config.SNAKE_PART_SIZE = config.GRID_CELL_SIZE
 config.FOOD_SIZE = config.GRID_CELL_SIZE
 config.SNAKE_MOVE_LENGTH = config.GRID_CELL_SIZE
@@ -28,7 +22,6 @@ snake_rect = None
 snake_length = None
 snake_parts = None
 snake_direction = None
-
 food_rect = None
 
 while running:
@@ -67,36 +60,30 @@ while running:
     time_now = pygame.time.get_ticks()
     screen.fill((173, 216, 230))
 
-    # Draw grid
     for i in range(0, screen_width, config.GRID_CELL_SIZE):
         pygame.draw.line(screen, config.GRID_COLOR, (i, 0), (i, screen_height))
         pygame.draw.line(screen, config.GRID_COLOR, (0, i), (screen_width, i))
 
-    # Update snake movement
     if time_now - time > config.DELAY:
         time = time_now
         snake_rect.move_ip(snake_direction)
         snake_parts.append(snake_rect.copy())
         snake_parts = snake_parts[-snake_length:]
 
-    # Draw food
     pygame.draw.rect(screen, config.SNAKE_FOOD_COLOR, food_rect, 0, 10)
 
-    # Draw snake
     for snake_part in snake_parts:
-        pygame.draw.rect(screen, (0, 0, 0), snake_part, 6,4)  # Black rectangle
+        pygame.draw.rect(screen, (0, 0, 0), snake_part, 6, 4)  # Black rectangle
         center_x = snake_part.centerx
         center_y = snake_part.centery
         circle_radius = config.GRID_CELL_SIZE // 4
         pygame.draw.circle(screen, (255, 255, 255), (center_x, center_y), circle_radius)
 
-        # Check for collisions
     if (snake_rect.left < 0 or snake_rect.right > screen_width or
-        snake_rect.top < 0 or snake_rect.bottom > screen_height or
-        len(snake_parts) != len(set(snake_part.center for snake_part in snake_parts))):
+            snake_rect.top < 0 or snake_rect.bottom > screen_height or
+            len(snake_parts) != len(set(snake_part.center for snake_part in snake_parts))):
         begin = True
 
-    # Check for food collision
     if snake_rect.center == food_rect.center:
         snake_length += 1
         bait = True
